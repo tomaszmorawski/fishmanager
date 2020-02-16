5773ea82cee8077ea3eece81be6fb6994d330012
Tomasz Mórawski <tomasz.morawski1987@gmail.com>
2020-02-16 14:56:21 +0100

    Initial Commit

---
+++ src/main/java/xyz/morawski/fishtankmanager/FishtankmanagerApplication.java
@@ -0,0 +1,13 @@
+package xyz.morawski.fishtankmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishtankmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishtankmanagerApplication.class, args);
+	}
+
+}
